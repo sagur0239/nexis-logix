f87bdb26929838105ca48737df4652303da7602f
Your Name <your@email.com>
2026-07-10 09:37:20 +0600

    Update project

--- invProject/settings.py
+++ invProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aw1ga7$)o+8nar80bnu&o2rl2cvs4^0zo&m$ein$(aqxbia81r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
